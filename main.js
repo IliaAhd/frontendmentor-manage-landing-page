e079ef5a7fe14488866b98ff31bc75d7a3a40406
IliaNoah <vaguaman86@gmail.com>
2022-11-23 19:24:14 +0330

    added header styles

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const navToggle = document.querySelector(".mobile-nav-button");
 const primaryNav = document.querySelector(".primary-navigation");
+const primaryHeader = document.querySelector(".primary-header");
+const iconHam = document.querySelector(".iconHam");
+const IconClose = document.querySelector(".icon-close");
 
 navToggle.addEventListener("click", function () {
-    primaryNav.classList.toggle('opened');
+  primaryNav.hasAttribute("data-visible")
+    ? navToggle.setAttribute("aria-expanded", false)
+    : navToggle.setAttribute("aria-expanded", true);
+  primaryNav.toggleAttribute("data-visible");
+  primaryHeader.toggleAttribute("data-overlay");
 });
