d387215b60dada7e8dd35f99a84cc425a06077d9
IliaNoah <vaguaman86@gmail.com>
2022-11-24 23:25:58 +0330

    fixed hamburger icon issues

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const navToggle = document.querySelector(".mobile-nav-button");
 const primaryNav = document.querySelector(".primary-navigation");
 const primaryHeader = document.querySelector(".primary-header");
-const iconHam = document.querySelector(".iconHam");
+const iconHam = document.querySelector(".icon-ham");
 const IconClose = document.querySelector(".icon-close");
 
 navToggle.addEventListener("click", function () {
@@ -10,4 +10,14 @@ navToggle.addEventListener("click", function () {
     : navToggle.setAttribute("aria-expanded", true);
   primaryNav.toggleAttribute("data-visible");
   primaryHeader.toggleAttribute("data-overlay");
+});
+
+iconHam.addEventListener("click", function () {
+  IconClose.style.display = 'block';
+  iconHam.style.display = 'none';
+});
+
+IconClose.addEventListener("click", function () {
+  IconClose.style.display = 'none';
+  iconHam.style.display = 'block';
 });
